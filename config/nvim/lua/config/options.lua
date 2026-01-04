6a5d32b8811a9b9887267778106f7c60abc6add8
Chris Lin <chrislin90210@gmail.com>
2026-01-03 21:55:29 -0600

    update

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -4,9 +4,11 @@ vim.opt.relativenumber = true
 vim.opt.cursorline = true
 
 vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
 vim.opt.sidescrolloff = 8
-vim.opt.smartindent = true
 vim.opt.autoindent = true
+vim.opt.smartindent = true
 
 vim.opt.incsearch = true
 vim.opt.hlsearch = false
